9ee69f0c527ef6ef354d3def4dd383efac803f0c
José <jose.silva.br@outlook.com>
2016-09-14 18:31:54 -0300

    Classe Log criada, mas não implementada.

---
+++ src/br/com/etec/log/Log.java
@@ -0,0 +1,21 @@
+/*
+ * Sistema de Log do projeto
+ */
+package br.com.etec.log;
+
+
+/**
+ *
+ * @author jose
+ */
+public class Log {
+
+    public static void erro(String erro, String classe) {
+    }
+
+    public static void atividades(String atividade, String classe) {
+    }
+
+    public static void usuarioLogin(String usuario, String perfil) {
+    }
+}
